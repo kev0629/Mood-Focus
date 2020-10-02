ed568f2e81d57b50558768e5e8e5a51c456bd3d7
kevin saffioti <kevin.saffioti@gmail.com>
2020-10-03 00:04:30 +0200

    Update ContentView.swift

--- Mood&Focus/ContentView.swift
+++ Mood&Focus/ContentView.swift
@@ -8,6 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    var body: some View {
+        TabView{
+            MoodView().tabItem { Text("Mood") }
+            FocusView().tabItem { Text("Focus") }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct MoodView: View {
     @State var sliderValue: Double = 5
     var body: some View {
         VStack {
@@ -34,17 +49,46 @@ struct ContentView: View {
             Spacer()
                 .frame(height: 20)
             Slider(value: $sliderValue, in: 0...10,step: 1)
-                    .padding()
-                    .background(Capsule().stroke(Color.gray, lineWidth: 2))
-                    .accentColor(Color.gray)
+                .padding()
+                .background(Capsule().stroke(Color.gray, lineWidth: 2))
+                .accentColor(Color.gray)
             Text("\(sliderValue, specifier: "%.0f")")
                 .font(.largeTitle)
-                }.padding()
+        }.padding()
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct FocusView: View {
+    @State var sliderValue: Double = 5
+    var body: some View {
+        VStack {
+            HStack{
+                Spacer()
+                Image("disiped")
+                    .resizable()
+                    .scaledToFit()
+                Image("hard")
+                    .resizable()
+                    .scaledToFit()
+                Image("boring")
+                    .resizable()
+                    .scaledToFit()
+                Image("okay")
+                    .resizable()
+                    .scaledToFit()
+                Image("concentrated")
+                    .resizable()
+                    .scaledToFit()
+                Spacer()
+                
+            }
+            Spacer()
+                .frame(height: 20)
+            Slider(value: $sliderValue, in: 0...10,step: 1)
+                .padding()
+                .background(Capsule().stroke(Color.gray, lineWidth: 2))
+                .accentColor(Color.gray)
+            Text("\(sliderValue, specifier: "%.0f")")
+                .font(.largeTitle)
+        }.padding()
     }
 }
