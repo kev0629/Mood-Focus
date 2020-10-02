f5a2d2fc773e384ccf86e978935aa6f8a333af7c
kevin saffioti <kevin.saffioti@gmail.com>
2020-10-02 16:25:27 +0200

    Initial Commit

---
+++ Mood&Focus/Mood_FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mood_FocusApp.swift
+//  Mood&Focus
+//
+//  Created by kevin saffioti on 02/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Mood_FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
